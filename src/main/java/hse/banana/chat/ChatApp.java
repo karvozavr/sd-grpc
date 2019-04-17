d4e0b41056f9296a896a6a7170d69c81b52629cc
Kostya Nedikov <konstantin.a.nedikov@gmail.com>
2019-04-17 15:06:16 +0300

    ui

--- src/main/java/hse/banana/chat/ChatApp.java
+++ src/main/java/hse/banana/chat/ChatApp.java
@@ -6,6 +6,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+
+import java.util.Optional;
 
 public class ChatApp extends Application {
     private static final int appWidth = 400;
@@ -14,6 +17,14 @@ public class ChatApp extends Application {
 
     private static Stage stage;
     private static Scene menuScene;
+
+    private String login;
+    private ButtonType buttonTypeServer = new ButtonType("Server");
+    private ButtonType buttonTypeClient = new ButtonType("Client");
+
+    enum ChoseType {
+        NONE, CLIENT, SERVER
+    }
 
     /**
      * Sets menu scene
@@ -43,6 +54,121 @@ public class ChatApp extends Application {
         stage.setHeight(appHeight);
         stage.setWidth(appWidth);
         stage.show();
+    }
+
+    @FXML
+    public void initialize() {
+        boolean success = false;
+        while (!success) {
+            Optional<String> result = createLoginDialog();
+            success = result.isPresent() && !result.get().isEmpty();
+            if (success) {
+                login = result.get();
+            } else {
+                createAlertDialog("empty name");
+            }
+        }
+
+        ChoseType type = ChoseType.NONE;
+        while (type == ChoseType.NONE) {
+            Optional<ButtonType> result = createServerClientDialog();
+            if (result.isPresent()) {
+                if (result.get() == buttonTypeClient) {
+                    type = ChoseType.CLIENT;
+                } else {
+                    type = ChoseType.SERVER;
+                }
+            } else {
+                System.exit(0);
+            }
+        }
+
+        if (type == ChoseType.CLIENT) {
+            startClient();
+        } else {
+            startServer();
+        }
+    }
+
+    private void startServer() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Waiting for client");
+        alert.setHeaderText(null);
+        alert.setContentText("");
+            boolean success = waitingConnect();
+        alert.showAndWait();
+    }
+
+    private void startClient() {
+        boolean success = false;
+        while (!success) {
+            Optional<String> result = createClientDialog();
+            if (!result.isPresent()) {
+                System.exit(0);
+            }
+            success = !result.get().isEmpty();
+            if (success) {
+                success = connectToHost(result.get());
+                if (!success) {
+                    createAlertDialog("can't connect");
+                }
+            } else {
+                createAlertDialog("empty name");
+            }
+        }
+    }
+
+    private boolean connectToHost(String host) {
+        return false;
+    }
+
+    private boolean waitingConnect() {
+        return false;
+    }
+
+    private Optional<String> createClientDialog() {
+        TextInputDialog dialog = new TextInputDialog("localhost");
+        dialog.setTitle("Choose server");
+        dialog.setHeaderText("");
+        dialog.setContentText("Please enter your server:");
+        return dialog.showAndWait();
+    }
+
+    private Optional<String> createLoginDialog() {
+        TextInputDialog dialog = new TextInputDialog("petuh");
+        dialog.setTitle("Login");
+        dialog.setHeaderText("");
+        dialog.setContentText("Please enter your login:");
+        return dialog.showAndWait();
+    }
+
+    private Optional<ButtonType> createServerClientDialog() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog with Custom Actions");
+        alert.setHeaderText("Look, a Confirmation Dialog with Custom Actions");
+        alert.setContentText("Choose your option.");
+
+        alert.getButtonTypes().setAll(buttonTypeServer, buttonTypeClient);
+        return alert.showAndWait();
+    }
+
+    private void createAlertDialog(String error) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("");
+        alert.setContentText(error);
+        alert.showAndWait();
+    }
+
+    @FXML
+    TextField textField;
+
+    @FXML
+    public void onSendButtonClick() {
+        String text = textField.getText();
+        if (!text.isEmpty()) {
+            System.out.println(text);
+        }
     }
 
     private Scene openScene(String sceneName) throws Exception {
