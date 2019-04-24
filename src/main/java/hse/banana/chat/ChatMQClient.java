e81d910a7b9b2e982bcacb2b0bc7fa245e5dc532
karvozavr <karvozavr@gmail.com>
2019-04-24 14:36:31 +0300

    Lolkek

---
+++ src/main/java/hse/banana/chat/ChatMQClient.java
@@ -0,0 +1,54 @@
+package hse.banana.chat;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class ChatMQClient {
+
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void send(String message) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+
+            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+
+    public static void receive() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+    }
+
+    public static void main(String[] args) throws Exception {
+        receive();
+        send("Message 1");
+        receive();
+
+        send("Message 2");
+    }
+}
---
+++ src/main/java/hse/banana/chat/RabbitMessenger.java
@@ -0,0 +1,62 @@
+package hse.banana.chat;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.nio.charset.StandardCharsets;
+import java.util.function.Consumer;
+
+@SuppressWarnings("Duplicates")
+public class RabbitMessenger extends AbstractMessenger {
+
+    private final String channelName;
+
+    public RabbitMessenger(String channelName) {
+        this.channelName = channelName;
+    }
+
+    @Override
+    void sendMessage(String login, String message) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(channelName, "fanout");
+
+            channel.basicPublish(channelName, "", null, (login + ": " + message).getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    void subcribe(Consumer<String> messageAccepter) {
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+
+            factory.setHost("localhost");
+
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.exchangeDeclare(channelName, "fanout");
+            String queueName = channel.queueDeclare().getQueue();
+            channel.queueBind(queueName, channelName, "");
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                System.out.println(" [x] Received '" + message + "'");
+                messageAccepter.accept(message);
+            };
+
+            channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
